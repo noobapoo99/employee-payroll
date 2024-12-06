cd6e68a6c3acf78a2260fc5ab5f10a8b45a42039
noobapoo99 <apoorvtripathi99@gmail.com>
2024-12-06 22:34:16 +0530

    coonstructor

---
+++ main.java
@@ -0,0 +1,16 @@
+abstract class Employee{
+    private String name;
+    private int id;
+    // constructors
+    public Employee(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+}
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
